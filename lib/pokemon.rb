fe3bf11906f432aa82fcbd00b7c5986f65ef7666
Eden Mesfun <emesfun@gmail.com>
2018-03-13 19:35:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,6 @@ class Pokemon
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
-    db.execute("ALTER TABLE pokemon ADD hp NOT NULL DEFAULT 60")
   end
 
   def self.find(id, db)
