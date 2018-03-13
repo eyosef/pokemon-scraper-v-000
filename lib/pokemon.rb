05fce9350a0d0e739c5c616d03084ea18d2db643
Eden Mesfun <emesfun@gmail.com>
2018-03-13 19:14:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
   end
 
   def self.find(name, db)
-    db.execute("SELECT * FROM pokemon where pokemon.name = ?", name)
+    db.execute("SELECT * FROM pokemon WHERE pokemon.name = ?", name)
   end
 
 
