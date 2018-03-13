243430b6a530291bd405fca9ace669f675d8d515
Eden Mesfun <emesfun@gmail.com>
2018-03-13 18:42:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,4 +11,8 @@ class Pokemon
   def self.save(id, name, type)
   end
 
+  def self.save(name, breed, age, database_connection)
+    database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+  end
+
 end
